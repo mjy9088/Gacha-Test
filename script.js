fcfe13ce2b1860ace5a1d377149e289bdc32b07b
맹주영 <mjy9088@naver.com>
2019-09-08 00:05:28 +0900

    log

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var have = 1000000;
 var played = 0;
 
+var win = 0, lose = 0;
+
 function play() {
     var good = Math.floor(Math.random() * 2);
+    var prev = have;
     have = Math.ceil(have * (0.75 + good * 0.55));
     played++;
     document.getElementById('have').value = have;
     document.getElementById('played').value = played;
+    var log = document.getElementById('log');
+    log.value = played + ' : ' + (good ? 'WIN! (' + ++win + '/' + lose + '' : 'LOSE (' + win + '/' + ++lose + '') + ') ' + prev + ' -> ' + have + '\n' + log.value;
 }
